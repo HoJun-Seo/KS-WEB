900869475e5a5890cf69fa3ecf6a0e43809ede9f
SeoHoJun <ghwns6659@naver.com>
2020-04-14 23:56:49 +0900

    View implementation(MVC pattern)

--- src/main/java/com/HoJun/app/controller/IndexController.java
+++ src/main/java/com/HoJun/app/controller/IndexController.java
@@ -5,6 +5,8 @@ import com.HoJun.app.domain.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class IndexController {
@@ -20,5 +22,32 @@ public class IndexController {
         Iterable<Student> students = studentRepository.findAll();
         model.addAttribute("Students", students);
         return "index";
+    }
+    @GetMapping("/add")
+    public String showAddForm(Student student) {
+        return "add-student";
+    }
+    @PostMapping("/add")
+    public String addStudent(Student student) {
+        studentRepository.save(student);
+        return "redirect:/";
+    }
+    @GetMapping("/update/{id}")
+    public String showUpdateForm(@PathVariable("id") Long id, Model model){
+        Student student = studentRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invaild student Id : " + id));
+                model.addAttribute("student", student);
+                return "update-student";
+    }
+    @PostMapping("/update/{id}")
+    public String updateStudent(Student student) {
+        studentRepository.save(student);
+        return "redirect:/";
+    }
+    @GetMapping("delete/{id}")
+    public String deleteStudent(@PathVariable("id") Long id, Model model) {
+        Student student = studentRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid student Id : " + id));
+        studentRepository.delete(student);
+        model.addAttribute("students", studentRepository.findAll());
+        return "redirect:/";
     }
 }
